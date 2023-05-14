8f2b64604afa78f3e164e8fb187140feaa13b0cf
NebulaDev <nebuladev.contact@gmail.com>
2023-05-14 17:13:03 +1200

    Add null type

--- Sprout.Tests/Subjects.cs
+++ Sprout.Tests/Subjects.cs
@@ -4,10 +4,9 @@ public class Subjects {
     [Fact]
     public void TestSubjects() {
         SPObject testObject = SP.FileHandle("/Users/nebuladev/Documents/sprout/Subjects/Test.sp");
-        string shouldObject = "{\\n    123 = 456;\\n    abc = def;\\n}";
-        Assert.Equal(testObject["array"]?[0], 1);
-        Assert.Equal(testObject["array"]?[1].ToString().Replace("\n", "\\n").Replace("\t", "\\t"), shouldObject);
+        
+        Assert.Equal(testObject["array"]?[0], null);
         Assert.Equal(testObject["array"]?[1]["123"], 456);
-        Assert.Equal(testObject["array"]?[2] + 1, 4);
+        Assert.Equal(testObject["array"]?[2] + 1, 2);
     }
 }
